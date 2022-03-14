6c2a1b89856e967896c0a747699afd40f3fa1938
Edmilson Silva <edmilson1406>
2022-03-14 08:07:51 -0300

    1403

--- verificaData.js
+++ verificaData.js
@@ -1,1 +1,30 @@
+var dataAtual = new Date() 
+
+var dataUser = new Date("2003-03-12")
+
+hoje = new Date();
+//console.log(dataUser.getMonth())
+
+console.log(dataAtual)
+console.log(dataUser)
+if ((dataAtual.getFullYear() - dataUser.getFullYear() == 18)) {
+    if ((dataUser.getMonth()+1) <= (dataAtual.getMonth()+1)){
+        if (dataUser.getDate() <= dataAtual.getDate()) {
+            console.log("Maior de Idade1")
+        }
+        else {
+            console.log("Menor de Idade1")
+        }
+    }
+    else {
+        console.log("Menor de Idade1")
+    }
+}
+else if((dataAtual.getFullYear() - dataUser.getFullYear() > 18)){
+    console.log("Maior de Idade")
+}
+
+else {
+    console.log("Menor de Idade")
+}
 
